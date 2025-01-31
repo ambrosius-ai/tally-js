c0778bbb18a1b61c8e41525329797957a73f3081
Joschwe <webmaster@schweizer-jonas.de>
2025-01-31 09:45:26 +0100

    added all remaining payload types

--- src/types/payload.dto.types.ts
+++ src/types/payload.dto.types.ts
@@ -1,3 +1,5 @@
+import { TallyBlockTypes } from '../lib'
+
 export type TallyPayloadDTO =
   | TallyPayloadLayoutDTO
   | TallyPayloadDividerDTO
@@ -84,9 +86,258 @@ export interface TallyPayloadEmbedDTO extends TallyPayloadLayoutDTO {
   type?: 'rich' | 'video' | 'photo' | 'link' | 'pdf' | 'gist' | 'image/*' | 'video/*' | 'audio/*'
   width?: string
 }
+
 export interface TallyPayloadEmbedAudioDTO extends TallyPayloadLayoutDTO {
   provider: string
   url: string
 }
 
 export type TallyPayloadEmbedVideoDTO = TallyPayloadEmbedAudioDTO
+
+export interface TallyPayloadDefaultAnswerDTO {
+  blockGroupUuid: string
+  questionType: TallyBlockTypes
+  type: 'InputField' | 'CalculatedField' | 'HiddenField'
+  uuid: string
+  calculatedFieldType?: 'NUMBER' | 'TEXT'
+  title?: string
+}
+export interface TallyPayloadQuestionDTO {
+  isHidden?: boolean
+  isRequired?: boolean
+}
+
+export interface TallyPayloadMultipleChoiceDTO extends TallyPayloadQuestionDTO {
+  allowMultiple?: boolean
+  badgeType?: 'OFF' | 'NUMBERS' | 'LETTERS'
+  color?: string
+  colorCodeOptions?: boolean
+  columnListUuid?: string
+  columnRatio?: number
+  columnUuid?: string
+  defaultAnswer?: TallyPayloadDefaultAnswerDTO
+  hasBadge?: boolean
+  hasDefaultAnswer?: boolean
+  hasMaxChoices?: boolean
+  hasMinChoices?: boolean
+  hasOtherOption?: boolean
+  image?: string
+  isOtherOption?: boolean
+  lockInPlace?: string[]
+  maxChoices?: number
+  minChoices?: number
+  name?: string
+  randomize?: boolean
+}
+
+export interface TallyPayloadCheckboxesDTO extends TallyPayloadQuestionDTO {
+  columnListUuid?: string
+  columnRatio?: number
+  columnUuid?: string
+  defaultAnswer?: TallyPayloadDefaultAnswerDTO
+  hasDefaultAnswer?: boolean
+  name?: string
+}
+
+export interface TallyPayloadDropdownDTO extends TallyPayloadCheckboxesDTO {
+  placeholder?: string
+}
+
+export interface TallyPayloadRankingDTO extends TallyPayloadCheckboxesDTO {}
+
+export interface TallyPayloadMatrixDTO extends TallyPayloadCheckboxesDTO {}
+
+export interface TallyPayloadMultiSelectDTO extends TallyPayloadCheckboxesDTO {
+  hasMaxChoices?: boolean
+  hasMinChoices?: boolean
+  maxChoices?: number
+  minChoices?: number
+}
+
+export interface TallyPayloadInputTextDTO extends TallyPayloadCheckboxesDTO {
+  hasMaxCaracters?: boolean
+  hasMinCaracters?: boolean
+  maxCharacters?: number
+  minCharacters?: number
+  placeholder?: string
+}
+
+export interface TallyPayloadInputNumberDTO extends TallyPayloadCheckboxesDTO {
+  currency?: 'USD' | 'EUR' | 'GBP' | 'CAD' | 'AUD' | 'JPY' | 'CNY' | 'INR'
+  decimalSeparator?: 'COMMA' | 'DOT'
+  defaultAnswer?: TallyPayloadDefaultAnswerDTO
+  hasDefaultAnswer?: boolean
+  hasMaxValue?: boolean
+  hasMinValue?: boolean
+  maxValue?: number
+  minValue?: number
+  numberFormat?: 'NUMBER' | 'CURRENCY' | 'PERCENTAGE'
+  placeholder?: string
+  thousandsSeparator?: 'COMMA' | 'DOT' | 'SPACE' | 'NONE'
+}
+export interface TallyPayloadInputEmailDTO extends TallyPayloadCheckboxesDTO {
+  placeholder?: string
+}
+
+export interface TallyPayloadInputLinkDTO extends TallyPayloadInputEmailDTO {}
+export interface TallyPayloadInputPhoneNumberDTO extends TallyPayloadInputEmailDTO {}
+
+export interface TallyPayloadInputDateDTO extends TallyPayloadInputEmailDTO {
+  disableDays?:
+    | 'MONDAY'
+    | 'TUESDAY'
+    | 'WEDNESDAY'
+    | 'THURSDAY'
+    | 'FRIDAY'
+    | 'SATURDAY'
+    | 'SUNDAY'
+    | 'PAST'
+    | 'FUTURE'
+    | 'TODAY'
+}
+
+export interface TallyPayloadInputTimeDTO extends TallyPayloadInputEmailDTO {}
+
+export interface TallyPayloadInputTextAreaDTO extends TallyPayloadInputTextDTO {}
+
+export interface TallyPayloadFileUploadDTO extends TallyPayloadQuestionDTO {
+  allowedFiles?: object
+  columnListUuid?: string
+  columnRatio?: number
+  columnUuid?: string
+  hasMaxFiles?: boolean
+  hasMaxFileSize?: boolean
+  maxFiles?: number
+  maxFileSize?: number
+  name?: string
+}
+
+export interface TallyPayloadLinearScaleDTO extends TallyPayloadCheckboxesDTO {
+  maxLabel?: string
+  maxValue?: number
+  minLabel?: string
+  minValue?: number
+}
+
+export interface TallyPayloadRatingDTO extends TallyPayloadCheckboxesDTO {
+  maxValue?: number
+}
+
+export interface TallyPayloadHiddenFieldsDTO {
+  columnListUuid?: string
+  columnRatio?: number
+  columnUuid?: string
+  fields?: object[]
+  isHidden?: boolean
+}
+
+export interface TallyPayloadDropdownOptionDTO extends TallyPayloadCheckboxesDTO {
+  index?: number
+  isFirst?: boolean
+  isLast?: boolean
+}
+
+export interface TallyPayloadRankingOptionDTO extends TallyPayloadDropdownOptionDTO {}
+
+export interface TallyPayloadMultipleChoiceOptionDTO extends TallyPayloadDropdownOptionDTO {
+  badgeType?: 'OFF' | 'NUMBERS' | 'LETTERS'
+  color?: string
+  colorcodeOptions?: boolean
+  hasBadge?: boolean
+  hasOtherOption?: boolean
+  image?: string
+  isOtherOption?: boolean
+}
+
+export interface TallyPayloadMultiSelectOptionDTO extends TallyPayloadMultipleChoiceOptionDTO {}
+
+export interface TallyPayloadCheckboxDTO extends TallyPayloadMultipleChoiceOptionDTO {}
+
+export interface TallyPayloadPaymentDTO extends TallyPayloadQuestionDTO {
+  columnListUuid?: string
+  columnRatio?: number
+  columnUuid?: string
+  isHidden?: boolean
+  isRequired?: boolean
+  name?: string
+  amount?: number
+  currency?: 'USD' | 'EUR' | 'GBP' | 'CAD' | 'AUD' | 'JPY' | 'CNY' | 'INR'
+}
+
+export interface TallyPayloadSignatureDTO extends TallyPayloadQuestionDTO {
+  columnListUuid?: string
+  columnRatio?: number
+  columnUuid?: string
+  name?: string
+}
+
+export interface TallyPayloadMatrixRowDTO extends TallyPayloadQuestionDTO {
+  columnListUuid?: string
+  columnRatio?: number
+  columnUuid?: string
+  name?: string
+  isFirst?: boolean
+  isLast?: boolean
+  index?: number
+}
+
+export interface TallyPayloadMatrixColumnDTO extends TallyPayloadMatrixRowDTO {}
+
+export interface TallyPayloadWalletConnectDTO extends TallyPayloadQuestionDTO {
+  columnListUuid?: string
+  columnRatio?: number
+  columnUuid?: string
+  name?: string
+}
+export interface TallyPayloadConditionalLogicDTO {
+  actions?: { blockUuids: string[]; type: 'SHOW' | 'HIDE' | 'SKIP_TO'; uuid: string }
+  columnListUuid?: string
+  columnRatio?: number
+  columnUuid?: string
+  conditionals?: {
+    field: {
+      blockGroupUuid: string
+      questionType: TallyBlockTypes
+      type: 'InputField' | 'CalculatedField' | 'HiddenField'
+      uuid: string
+      calculatedFieldType: ' NUMBER ' | 'TEXT'
+      title: string
+    }
+    operator: string
+    uuid: string
+    value: string
+  }
+  isHidden?: boolean
+}
+
+export type TallyField = {
+  name: string
+  type: 'NUMBER' | 'TEXT'
+  uuid: string
+  value: string
+}
+
+export interface CalculdatedFieldsDTO {
+  columnListUuid?: string
+  columnRatio?: number
+  columnUuid?: string
+  fields: TallyField
+  isHidden?: boolean
+}
+
+export interface TallyPayloadCaptchaDTO {
+  columnListUuid?: string
+  columnRatio?: number
+  columnUuid?: string
+  isHidden?: boolean
+  isRequired?: boolean
+}
+
+export interface TallyPayloadRespondentCountryDTO {
+  columnListUuid?: string
+  columnRatio?: number
+  columnUuid?: string
+  isHidden?: boolean
+  isRequired?: boolean
+  name?: string
+}
